8b2a46a4c2240d17ab925b686882015d634ae401
LJ <ljdb1999@gmail.com>
2025-04-24 15:50:38 -0700

    Working on footer/newsletter

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <Container>
+        <Row className="align-item-center">
+            <Col sm={6}>
+
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default Footer
