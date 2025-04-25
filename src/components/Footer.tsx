10c67fbe781d9106a27cca4ddc42cf7afb7e0656
LJ <ljdb1999@gmail.com>
2025-04-24 23:49:19 -0700

    Got newsletter to work!

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import logo from "../assets/img/logo.svg"
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
+import { MailchimpForm } from './MailchimpForm'
+
 
 type Props = {}
 
 const Footer = (props: Props) => {
+
+  console.log(`${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`)
+
   return (
     <Container>
-        <Row className="align-item-center">
-            <Col sm={6}>
-
-            </Col>
+        <Row>
+          <MailchimpForm/>
+        </Row>
+        <Row className='justify-content-center'>
+          <img src={logo} alt="Logo" style={{ width: '50%', height: 'auto' }}/>
+        </Row>
+        <Row>
+          <div className="social-icon">
+            <a href=""><img src={navIcon1} /></a>
+            <a href=""><img src={navIcon2} /></a>
+            <a href=""><img src={navIcon3} /></a>
+          </div>
+          <p>CopyRight 2025. All Rights Reserved by Lawrence John Balanza</p>
         </Row>
     </Container>
   )
---
+++ src/components/Newsletter.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+    return (
+    <Col leg={12}>
+        <div className="newsletter-bx">
+            <Row>
+                <Col lg={12} md={6} xl={5}>
+                    <h3>Subscribe to our Newsletter</h3>
+                    {status === 'sending' && <Alert>Sending...</Alert>}
+                    {status === 'error' && <Alert variant="danger">{message}</Alert>}
+                    {status === 'success' && <Alert variant="success">{message}</Alert>}
+                </Col>
+                <Col md={6} xl={7}>
+                    <form onSubmit={handleSubmit}>
+                    <Row className="align-items-center">
+                        <div className="new-email-bx justify-content-center">
+                            <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                        </div>
+                    </Row>
+                    <Row className="align-items-center">
+                        <button type="submit">Submit</button>
+                    </Row>
+                    </form>
+                </Col>
+            </Row>
+        </div>
+    </Col>
+  )
+}
+
+export default Newsletter
