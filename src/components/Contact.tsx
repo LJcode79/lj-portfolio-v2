97bb3161724c5d6d1ff5b06663041abcfb03ee6f
LJ <ljdb1999@gmail.com>
2025-05-02 00:34:46 -0700

    url update 3

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,7 +34,7 @@ const Contact = (props: Props) => {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
         setButtonText('Sending...');
-        let response = await fetch("https://portfolio-backend3-30ai0jz93-lawrence-johns-projects.vercel.app/contact", {
+        let response = await fetch("https://portfolio-backend3-pi.vercel.app//contact", {
         // let response = await fetch("http://localhost:5000/contact/", {
             method: "POST",
             headers: {
